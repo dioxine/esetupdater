c24d09f451300cf48f37f9e555c37b5d800e7eda
DioXine <dioxine@mail.ru>
2024-09-13 00:12:37 +1000

    Few fixes

--- internal/update_main.go
+++ internal/update_main.go
@@ -9,16 +9,15 @@ func UpdateMain(sectionBlockArrayOld []SectionBlock, sectionBlockArrayNew []Sect
 
 		// Full download toggle
 		if fullDownload {
-			fmt.Println("Section: " + sectionBlock["sectionName"])
+			fmt.Println("Section: " + sectionBlock["sectionName"] + " full downloading")
 			DownloadFile(sectionBlock["file"])
-
 			// Check if does not exist
 		} else if !CheckNupExist(sectionBlock["file"]) {
-			fmt.Println("Section: " + sectionBlock["sectionName"])
+			fmt.Println("Section: " + sectionBlock["sectionName"] + " downloading")
 			DownloadFile(sectionBlock["file"])
 			// Version differs check
 		} else if sectionBlock["versionid"] != sectionBlockArrayOld[idx]["versionid"] {
-			fmt.Println("Section: " + sectionBlock["sectionName"])
+			fmt.Println("Section: " + sectionBlock["sectionName"] + " downloading delta")
 			fmt.Println("VersionID differs! Old: " + sectionBlockArrayOld[idx]["versionid"] + " New: " + sectionBlock["versionid"])
 			DownloadFile(sectionBlock["file"])
 		}
