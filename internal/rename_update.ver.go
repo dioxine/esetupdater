3b9cbadc630d187145f3322c7acf005173626d04
DioXine <dioxine@mail.ru>
2024-09-13 01:03:23 +1000

    Fixed windows paths in rename module

--- internal/rename_update.ver.go
+++ internal/rename_update.ver.go
@@ -13,19 +13,19 @@ func RenameUpdateVer() error {
 		log.Fatalln(err)
 	}
 
-	if _, err := os.Stat(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath) + "/dll/update.ver"); err == nil {
-		file, err := os.Open(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath) + "/dll/update.ver")
+	if _, err := os.Stat(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath)[1:] + filepath.Clean("/dll/update.ver")); err == nil {
+		file, err := os.Open(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath)[1:] + filepath.Clean("/dll/update.ver"))
 		if err != nil {
 			log.Fatalln(err)
 		}
 
-		defer file.Close()
 		// Check file size > 0
 		if info, err := file.Stat(); err == nil && info.Size() > 0 {
 			// Rename update.ver to update.old
-			os.Remove(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath) + "/dll/update.old")
+			os.Remove(filepath.Clean(config.Local.RootPath) + filepath.Clean(config.Remote.RootPath)[1:] + filepath.Clean("/dll/update.old"))
 
-			if err = os.Rename(filepath.Clean(config.Local.RootPath)+filepath.Clean(config.Remote.RootPath)+"/dll/update.ver", filepath.Clean(config.Local.RootPath)+filepath.Clean(config.Remote.RootPath)+"/dll/update.old"); err != nil {
+			if err = os.Rename(filepath.Clean(config.Local.RootPath)+filepath.Clean(config.Remote.RootPath)[1:]+filepath.Clean("/dll/update.ver"), filepath.Clean(config.Local.RootPath)+filepath.Clean(config.Remote.RootPath)[1:]+filepath.Clean("/dll/update.old")); err != nil {
+				file.Close()
 				log.Fatalln(err)
 			}
 		}
