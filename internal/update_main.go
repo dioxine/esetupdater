0e73c1813cef79320387c9a3bab452f6d759a1f2
DioXine <dioxine@mail.ru>
2024-09-12 23:59:52 +1000

    Working on windows compatibility. Some refactoring

--- internal/update_main.go
+++ internal/update_main.go
@@ -9,22 +9,17 @@ func UpdateMain(sectionBlockArrayOld []SectionBlock, sectionBlockArrayNew []Sect
 
 		// Full download toggle
 		if fullDownload {
-			fmt.Println(sectionBlock["file"])
+			fmt.Println("Section: " + sectionBlock["sectionName"])
 			DownloadFile(sectionBlock["file"])
 
 			// Check if does not exist
 		} else if !CheckNupExist(sectionBlock["file"]) {
 			fmt.Println("Section: " + sectionBlock["sectionName"])
-			fmt.Println("File does not exist on place. Downloading New file: " + sectionBlock["file"][1:])
-			fmt.Println(sectionBlock["file"])
 			DownloadFile(sectionBlock["file"])
-
 			// Version differs check
 		} else if sectionBlock["versionid"] != sectionBlockArrayOld[idx]["versionid"] {
 			fmt.Println("Section: " + sectionBlock["sectionName"])
 			fmt.Println("VersionID differs! Old: " + sectionBlockArrayOld[idx]["versionid"] + " New: " + sectionBlock["versionid"])
-			fmt.Println("Downloading New file: " + sectionBlock["file"][1:])
-			fmt.Println(sectionBlock["file"])
 			DownloadFile(sectionBlock["file"])
 		}
 	}
